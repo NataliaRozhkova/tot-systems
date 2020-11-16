48728773e28a799250f9bbd15347648402f838e8
Natasha <nat.n.rozhkova@gmail.com>
2020-11-16 17:24:53 +0500

    Commit

--- src/main/java/moscow/exchange/server/handlers/GetSecurityHandler.java
+++ src/main/java/moscow/exchange/server/handlers/GetSecurityHandler.java
@@ -6,9 +6,8 @@ import moscow.exchange.data.Response;
 import moscow.exchange.data.entity.Security;
 import moscow.exchange.data.repository.Repository;
 
-import java.io.IOException;
 
-public class GetSecurityHandler extends BaseHandler<String,Security> implements HttpHandler {
+public class GetSecurityHandler extends BaseHandler<String, Security> implements HttpHandler {
 
     private final Repository repository;
 
@@ -17,10 +16,9 @@ public class GetSecurityHandler extends BaseHandler<String,Security> implements 
     }
 
     @Override
-    String handleGetRequest(HttpExchange httpExchange) throws IOException {
-        String[] httpExchangeParameters = httpExchange.getRequestURI().getQuery().split("=");
-        String secidValue = httpExchangeParameters[1];
-        return secidValue;
+    String handleGetRequest(HttpExchange httpExchangeParameters) {
+        String[] httpExchange = httpExchangeParameters.getRequestURI().getQuery().split("=");
+        return httpExchange[1];
     }
 
     @Override
@@ -38,8 +36,7 @@ public class GetSecurityHandler extends BaseHandler<String,Security> implements 
         if (response.state == Response.State.ERROR) {
             return "Security not found";
         }
-        return new StringBuilder()
-                .append(TableDate.SECURITY_TABLE_HEAD)
-                .append(response.body.toStringXml()).append(TableDate.FINISH_TABLE).toString();
+        return TableDate.SECURITY_TABLE_HEAD +
+                response.body.toStringXml() + TableDate.FINISH_TABLE;
     }
 }
