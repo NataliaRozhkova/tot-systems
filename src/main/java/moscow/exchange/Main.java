06a557c8085e46a08c3bfff26a87f173dce83403
Natasha <nat.n.rozhkova@gmail.com>
2020-11-17 14:45:34 +0500

    Commit

--- src/main/java/moscow/exchange/Main.java
+++ src/main/java/moscow/exchange/Main.java
@@ -1,18 +1,11 @@
 package moscow.exchange;
 
 import moscow.exchange.data.db.ExchangeDataSource;
-import moscow.exchange.data.entity.Security;
 import moscow.exchange.data.entity.Transaction;
-import moscow.exchange.data.entity.parser.SecurityParser;
-import moscow.exchange.data.entity.parser.TransactionParser;
 import moscow.exchange.data.repository.Repository;
 import moscow.exchange.server.ExchangeHttpServer;
 
 import java.io.*;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -36,14 +29,6 @@ public class Main {
         ExchangeHttpServer server = new ExchangeHttpServer(host, port, new Repository(new ExchangeDataSource()));
         server.start();
 
-
-//        ExchangeDataSource dataSource = new ExchangeDataSource();
-//        ArrayList<Transaction> securities = (ArrayList<Transaction>) dataSource.readAllTransactions().body;
-//        StringBuilder builder = new StringBuilder();
-//        for (Transaction s : securities) {
-//            builder.append(s.toString() + "\n");
-//        }
-//        System.out.println(builder.toString());
 
 
     }
