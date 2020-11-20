68b7d601becc07ac953d7e26f110f5d01d16d507
Natasha <nat.n.rozhkova@gmail.com>
2020-11-20 13:09:15 +0500

    SecurityHandler change

--- src/main/java/moscow/exchange/server/handlers/SecurityHandler.java
+++ src/main/java/moscow/exchange/server/handlers/SecurityHandler.java
@@ -2,41 +2,22 @@ package moscow.exchange.server.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import moscow.exchange.FileReader;
 
 import java.io.*;
 
 public class SecurityHandler implements HttpHandler {
-    String htmlPage = getFile();
+
+    private final String SECURITY_HTML_PAGE = "src/main/resources/security.html";
+    String htmlPage = FileReader.getFile(SECURITY_HTML_PAGE);
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        handleResponse(exchange);
-    }
-
-    private void handleResponse(HttpExchange httpExchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
         OutputStream outputStream = httpExchange.getResponseBody();
         httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
         httpExchange.sendResponseHeaders(200, htmlPage.getBytes().length);
         outputStream.write(htmlPage.getBytes());
         outputStream.flush();
         outputStream.close();
-    }
-
-    private static String getFile() {
-        BufferedReader reader;
-        StringBuilder html = new StringBuilder();
-        try {
-            reader = new BufferedReader(new FileReader(new File("src/main/resources/security.html")));
-            String line = reader.readLine();
-            while (line != null) {
-                html.append(line);
-                line = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return html.toString();
     }
 }
