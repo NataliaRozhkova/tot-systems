6b1d5eaada36873014f034ad3fae8b6c3598743b
Natasha <nat.n.rozhkova@gmail.com>
2020-11-20 13:11:52 +0500

    StartServiceHandler change

--- src/main/java/moscow/exchange/server/handlers/StartServiceHandler.java
+++ src/main/java/moscow/exchange/server/handlers/StartServiceHandler.java
@@ -1,42 +1,24 @@
 package moscow.exchange.server.handlers;
 
+import moscow.exchange.FileReader;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
 import java.io.*;
 
 public class StartServiceHandler implements HttpHandler {
 
-    String htmlPage = getFile();
+    private final String START_SERVICE_HTML_PAGE = "src/main/resources/start_service.html";
+
+    String htmlPage = FileReader.getFile(START_SERVICE_HTML_PAGE);
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        handleResponse(exchange);
-    }
-
-    private void handleResponse(HttpExchange httpExchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
         OutputStream outputStream = httpExchange.getResponseBody();
         httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
         httpExchange.sendResponseHeaders(200, htmlPage.getBytes().length);
         outputStream.write(htmlPage.getBytes());
         outputStream.flush();
         outputStream.close();
-    }
-
-    private static String getFile() {
-        BufferedReader reader;
-        StringBuilder html = new StringBuilder();
-        try {
-            reader = new BufferedReader(new FileReader(new File("src/main/resources/start_service.html")));
-            String line = reader.readLine();
-            while (line != null) {
-                html.append(line);
-                line = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return html.toString();
     }
 }
